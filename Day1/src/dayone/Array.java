1ea2b307d18166b3901bd18ed4455b3729e0a80f
Karthi <karthik raja@LAPTOP-08RA1148.(none)>
2025-03-18 10:00:41 +0530

    array methods

--- Day1/src/dayone/Array.java
+++ Day1/src/dayone/Array.java
@@ -1,5 +1,15 @@
 package dayone;
-
+import java.util.*;
 public class Array {
-
+public static void main(String[]args) {
+	int[] source= {30,20,40,10,50};
+	int[]destination=new int[5];
+	//copy
+	System.arraycopy(source, 0, destination, 0, source.length);
+	for(int n:destination) {
+		System.out.println(n+"");
+	}
+	Arrays.sort(source);
+	System.out.println("Sorted Array : "+Arrays.toString(source));
+}
 }

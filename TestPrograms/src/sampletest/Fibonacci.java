8d744e450e1c4946d978731e80124f9f03b44e49
Karthi <karthik raja@LAPTOP-08RA1148.(none)>
2025-03-18 14:54:54 +0530

    revised fibonaaci

--- TestPrograms/src/sampletest/Fibonacci.java
+++ TestPrograms/src/sampletest/Fibonacci.java
@@ -4,12 +4,13 @@ public class Fibonacci {
 
 	public static void main(String[] args) {
 		int n=10;
-		int n1=0,n2=1;
+		int n1=-1,n2=1;
 		for(int i=0;i<=n;i++) {
-			System.out.println(n1+" ");
+			
 			int next = n1+n2;
 			n1=n2;
 			n2=next;
+			System.out.println(n1);
 		}
 
 	}
